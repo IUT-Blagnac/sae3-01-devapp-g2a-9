26cc90551858922426b90c6dfc90b31fc9d36dda
Anxton <anxton4@gmail.com>
2022-12-14 15:23:52 +0100

    recuperation des donnees selectionees dans le config

--- iot/sae_mqtt.py
+++ iot/sae_mqtt.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 import paho.mqtt.client as mqtt
 import os, signal
@@ -8,20 +9,20 @@ mqttserver = "chirpstack.iut-blagnac.fr"
 mqttport = 1883
 
 # le fichier config.json sera donné par le programme java
-f = os.open('./config.json', os.O_RDONLY) #ouverture d'un fichier .json en lecture
+f = os.open(sys.path[0]+'/config.json', os.O_RDONLY) #ouverture d'un fichier .json en lecture
 config_raw = os.read(f, 1024) #nous allons lire 1024 bytes du fichier f.json
 config = json.loads(config_raw) #json est un format utilisé pour stocker et echanger des données il prend en paramètre un tableau de bytes cette fonction retourne un objet python
-f.close()
 
 data_waiting = False
+output = {}
 
 def get_data(mqtt, obj, msg):
     print('data') #affichage pour informer l'utilisateur
     global jsonMsg #creation d'une variable globale
     jsonMsg = json.loads(msg.payload) #recuperation de tout ce qui est envoyé par les capteurs en un format lisible par python
-    for data in config['data']:
-        value = jsonMsg["object"][data]
-        print(f"{data}: {value}") #affichage sous forme d'un dictionnaire
+    for data in jsonMsg['object']:
+        if data in config['data']:
+            output[data] = (jsonMsg["object"][data], jsonMsg["object"][data] > config['data'][data] if config['data'][data] != None else False)
     global data_waiting #reinitialiser la variable pour que la modification soit effective en dehors de la fonction
     data_waiting = True #changement de la variable globale data_waiting depuis la fonction
 
@@ -30,7 +31,7 @@ def ecriture(numero, frame):
     signal.alarm(30) #la fonction sera rappelée dans 30 secondes
     try:
         if data_waiting: #si le boolean global data_waiting est True
-            msg = json.dumps(jsonMsg["object"])
+            msg = json.dumps(output)
             print("écriture de :\n" + msg)
             fd = os.open(config['filename'], os.O_WRONLY|os.O_CREAT|os.O_TRUNC) # ecrit le fichier qui sera indiqué par java
             os.write(fd, msg.encode()) # encode en UTF-8 par defaut
@@ -52,7 +53,7 @@ for device in config["devices"]: #ref liste dans le fichier json
 
 client.subscribe(devices) #(si on veut recup toutes les données on met un "+") on en fait un seul appel pour s'abonner aux topics (lien) présents dans la liste devices
 
-client.on_message = get_data() #chaque message reçu appelera la fonction get_data()
+client.on_message = get_data #chaque message reçu appelera la fonction get_data()
 
 client.loop_forever() #boucle tant que l'on ne stoppe pas le programme de force
 
