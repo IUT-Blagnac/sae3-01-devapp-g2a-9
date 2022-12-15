6115f96ede05cbf3df36d5cae124c98e5f6d30de
Anxton <anxton4@gmail.com>
2022-12-15 17:33:46 +0100

    refactoring du site web, architecture + cleanup

---
+++ pages_web/notreEntreprise.php
@@ -0,0 +1,28 @@
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Commerce de la rue</title>
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="icon" type="image/x-icon" href="logo.ico">
+    </head>
+    <body>
+        <div class="content">
+            <?php include("header.html"); ?>
+            <main>
+                <div class="main-card">
+                    <h2>Notre entreprise</h2>
+                    <p>Paragraphe explicatif</p>
+                </div>
+                <div class="main-card">
+                    <h2>Actualité</h2>
+                    <p>Paragraphe explicatif</p>
+                </div>
+            </main>
+            <?php include("footer.html"); ?>
+        </div>
+        <div class="background">
+            <div class="shape"></div>
+            <div class="shape"></div>
+        </div>
+    </body>
+</html>
