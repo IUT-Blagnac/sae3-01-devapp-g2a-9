62e47950829823f4966a36db6c67e8ce8c322145
L-Yvelin <louisyvelin34720@gmail.com>
2023-01-03 14:42:39 +0100

    Ajout page inscription

--- php/Inscription.php
+++ php/Inscription.php
@@ -16,7 +16,7 @@ if (isset($valider)) {
         $_SESSION["autoriser"] = "oui";
         header("Location: index.php");
     } else {
-        $erreur = "mauvais login ou mdp";
+        $erreur = "Email incorrect ou déjà assignée";
     }
 }
 ?>
@@ -36,24 +36,18 @@ if (isset($valider)) {
             <?php $page = strtolower(basename(__FILE__, '.php')); include("include/header.php"); ?>
             <main>
                 <form method="post">
-                    <h3>Connexion Utilisateur</h3>
+                    <h3>Inscription Utilisateur</h3>
                     <label for="email">Email</label>
                     <input type="email" name="email" placeholder="sushi18@pouet.com" value="<?php echo isset($_COOKIE['email']) ? $_COOKIE['email'] : ''; ?>" id="email"/>
                     <label for="password">Mot de passe</label>
                     <input type="password" name="password" placeholder="∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗" id="password"/>
-                    <a id="mdp_oublie" href="reset_mdp.php">Mot de passe oublié</a>
                     
-                    <label class="checkbox-label" for="souvenir">
-                        <input type="checkbox" name="souvenir" id="souvenir">
-                        Se souvenir de moi
-                    </label>
+                    <button type="submit" name="valider">Inscription</button>
 
-                    <button type="submit" name="valider">Connexion</button>
+                    <?php echo isset($erreur) ? "<p id=\"erreur_connexion\">Adresse email déjà utilisée</p>" : '';?>
 
-                    <?php echo isset($erreur) ? "<p id=\"erreur_connexion\">Le mot de passe ou l'email est incorrect</p>" : '';?>
-
-                    <a class="social" href="Inscription.php">
-                        <div class="alternate">Inscription</div>
+                    <a class="social" href="Connexion.php">
+                        <div class="alternate">Connexion</div>
                     </a>
                 </form>
             </main>
