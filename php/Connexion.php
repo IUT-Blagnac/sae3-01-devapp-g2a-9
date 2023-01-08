b8b6a35393a406fe7a1891da75d9e5ad93ffe5f5
UneSo6 <unconfigured@null.spigotmc.org>
2023-01-08 14:31:03 +0100

    First try SGBD request

--- php/Connexion.php
+++ php/Connexion.php
@@ -10,13 +10,41 @@ if (isset($valider)) {
               (SID = db11g)
             )
           )" ;
-    $connect = oci_connect("SAEBD09", "M0ntBlanc1UT", $db);
+    $conn = oci_connect("SAEBD09", "M0ntBlanc1UT", $db);
 
     // Affichage d'erreur si la connexion échoue
-    if (!$connect) {
+    if (!$conn) {
         $e = oci_error();
         trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
     }
+
+    // Prepare the statement
+    $stid = oci_parse($conn, 'SELECT EMAILUSER FROM UTILISATEUR');
+    if (!$stid) {
+        $e = oci_error($conn);
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    // Perform the logic of the query
+    $r = oci_execute($stid);
+    if (!$r) {
+        $e = oci_error($stid);
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    // Fetch the results of the query
+    print "<table border='1'>\n";
+    while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        print "<tr>\n";
+        foreach ($row as $item) {
+            print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        }
+        print "</tr>\n";
+    }
+    print "</table>\n";
+
+    oci_free_statement($stid);
+    oci_close($conn);
 
     // SELECT emailUser, mdpUser FROM User WHERE emailUser = ? 
     // bind $email
