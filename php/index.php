67cab1a3daabec16c3e4a1ffb35d5d8f11645979
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-09 11:02:49 +0100

    ajout des images sur devweb + php index

--- php/index.php
+++ php/index.php
@@ -1,3 +1,28 @@
+<?php
+    session_start();
+    $db = "(DESCRIPTION =
+                (ADDRESS = (PROTOCOL = TCP)(HOST = oracle.iut-blagnac.fr)(PORT = 1521))
+                (CONNECT_DATA =
+                  (SERVER = DEDICATED)
+                  (SID = db11g)
+                )
+              )";
+    $conn = oci_connect("SAEBD09", "M0ntBlanc1UT", $db);
+
+    //EMAIL
+    $query = "SELECT nomcat FROM categorie";
+    $stid = oci_parse($conn, $query);
+    oci_execute($stid);
+
+    while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+        $res[] = $row['nomcat'];
+    }
+    //print(password_hash("aB12345", PASSWORD_DEFAULT)); // Pour hasher un mdp
+
+    oci_free_statement($stid);
+    oci_close($conn);
+?>
+
 <html lang="fr">
     <head>
         <meta charset="utf-8"/>
@@ -13,11 +38,16 @@
                 <div class="main-card">
                     <h2>Catégories</h2>
                     <div class="main-card-content">
-                        <a href="Categories.php?cat=cpu" class="categorie"><button>Processeurs</button></a>
+                        <?php
+                            foreach ($res as $categorie){
+                            echo " <a href=\"Categories.php?cat=" . $categorie . "\" class=\"categorie\"><button>" . ucfirst($categorie) . "</button></a>";
+                            }
+                        ?>
+                        <!-- <a href="Categories.php?cat=cpu" class="categorie"><button>Processeurs</button></a>
                         <a href="Categories.php?cat=hdd" class="categorie"><button>Disques durs</button></a>
                         <a href="Categories.php?cat=gpu" class="categorie"><button>Cartes graphiques</button></a>
                         <a href="Categories.php?cat=mb" class="categorie"><button>Cartes mères</button></a>
-                        <a href="Categories.php?cat=ventirad" class="categorie"><button>Ventilateurs</button></a>
+                        <a href="Categories.php?cat=ventirad" class="categorie"><button>Ventilateurs</button></a> -->
                     </div>
                 </div>
                 <div class="main-card">
