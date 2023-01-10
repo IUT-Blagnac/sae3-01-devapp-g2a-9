9d747fd858878e305cd11491e8e49ac381232ab6
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-10 15:00:07 +0100

    Debug recherche

--- php/Recherche.php
+++ php/Recherche.php
@@ -6,190 +6,54 @@
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     </head>
     <body>
+        <?php
+            $recherche =  htmlspecialchars($_GET['recherche']);
+            if (isset($recherche)){
+                session_start();
+                $db = "(DESCRIPTION =
+                            (ADDRESS = (PROTOCOL = TCP)(HOST = oracle.iut-blagnac.fr)(PORT = 1521))
+                            (CONNECT_DATA =
+                              (SERVER = DEDICATED)
+                              (SID = db11g)
+                            )
+                          )";
+                $conn = oci_connect("SAEBD09", "M0ntBlanc1UT", $db);
+                //requete sql
+                $query = "SELECT NOMPRODUIT, IDPRODUIT, PRIXPRODUIT, (PRIXPRODUIT - REDUCTION) as REDUC FROM produit WHERE NOMPRODUIT =".$recherche;
+                $stid = oci_parse($conn, $query);
+                oci_execute($stid);
+                while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+                    $res[] = ['nom' => $row['NOMPRODUIT'], 'id'=> $row['IDPRODUIT'], 'prix' => $row['PRIXPRODUIT'], 'reduc' => $row['REDUC']];
+                }
+                oci_free_statement($stid);
+                oci_close($conn);
+            }
+        ?>
         <div class="content">
             <?php $page = strtolower(basename(__FILE__, '.php')); include("include/header.php"); ?>
             <main>
                 <div class="barre-de-recherche">
                     <div class="barre-de-recheche">
                         <label for="barre-de-recherche">Rechercher</label>
-                        <input type="text" placeholder="Nom du produit">
+                        <form action="Recherche.php" method="get">
+                            <input type="text" name="recherche" placeholder="Nom du produit">
+                        </form>
                     </div>
                 </div>
                 <div class="main-card">
                     <h2>Ventilateurs</h2>
                     <div class="main-card-content">
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>                            
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>                            
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>                            
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>                            
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>                            
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                    </div>
+                    <?php
+                            foreach($res as $produit) { 
+                                echo" <div class=\"produit\">
+                                <div><a><strong>".$produit['nom']."</strong></a></div>
+                                <div class=\"image-produit-content\"><img class=\"image-produit\"src=\"./img/produits/".$produit['id']."_1.jpg\" alt=\"Image du produit\"></div>
+                                <div><a>".$produit['prix']." €</a></div>
+                                <div><a href=\"produit.php\"><button>Acheter</button></a></div>
+                                </div>";
+                            }
+                        ?>
                 </div>
-                <div class="main-card">
-                    <h2>Processeurs</h2>
-                    <div class="main-card-content">
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="main-card">
-                    <h2>Disque dur</h2>
-                    <div class="main-card-content">
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="main-card">
-                    <h2>Cartes graphiques</h2>
-                    <div class="main-card-content">
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                        <div class="produit">
-                            <div><a>Nom produit</a></div>
-                            <div><a>Image</a></div>
-                            <div><a>Prix</a></div>
-                            <div><a href="produit.php"><button>Acheter</button></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="main-card">
-                        <h2>Cartes mères</h2>
-                        <div class="main-card-content">
-                            <div class="produit">
-                                <div><a>Nom produit</a></div>
-                                <div><a>Image</a></div>
-                                <div><a>Prix</a></div>
-                                <div><a href="produit.php"><button>Acheter</button></a></div>
-                            </div>
-                            <div class="produit">
-                                <div><a>Nom produit</a></div>
-                                <div><a>Image</a></div>
-                                <div><a>Prix</a></div>
-                                <div><a href="produit.php"><button>Acheter</button></a></div>
-                            </div>
-                            <div class="produit">
-                                <div><a>Nom produit</a></div>
-                                <div><a>Image</a></div>
-                                <div><a>Prix</a></div>
-                                <div><a href="produit.php"><button>Acheter</button></a></div>
-                            </div>
-                            <div class="produit">
-                                <div><a>Nom produit</a></div>
-                                <div><a>Image</a></div>
-                                <div><a>Prix</a></div>
-                                <div><a href="produit.php"><button>Acheter</button></a></div>
-                            </div>
-                            <div class="produit">
-                                <div><a>Nom produit</a></div>
-                                <div><a>Image</a></div>
-                                <div><a>Prix</a></div>
-                                <div><a href="produit.php"><button>Acheter</button></a></div>
-                            </div>
-                        </div>
-                    </div>
             </main>
             <?php include("include/footer.html"); ?>
         </div>
