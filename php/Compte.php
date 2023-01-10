65db00a623883d544b0e613313dc7c94eb722997
L-Yvelin <louisyvelin34720@gmail.com>
2023-01-10 11:48:24 +0100

    delete cb

--- php/Compte.php
+++ php/Compte.php
@@ -77,11 +77,15 @@ while($row = oci_fetch_array($stid, OCI_ASSOC)){
                     </div>
                 </div>
                 <div class="main-card">
+                    
                     <h1  style="text-align: center">Méthodes de paiement <span style="position:relative; top: -.1em;">💳</span></h1>
                     <div class="zone-utilisateur">
                         <div class="bulle">
                             <div class="carte-bancaire">
-                            
+                                <form method="post" style="all: none;">
+                                    <input type="hidden" name="idcb" value="<?php echo $cb['idCB']?>">
+                                    <input type="submit" name="delCB" value="🗑️" style="all: none;">
+                                </form>
                                 <label for="nom-carte-bancaire" style="margin-top:0;">Nom</label>
                                 <input value="Demeyere" id="nom-carte-bancaire" disabled/>
 
@@ -90,42 +94,6 @@ while($row = oci_fetch_array($stid, OCI_ASSOC)){
                             
                                 <label for="cryptogramme-carte-bancaire">Cryptogramme visuel</label>
                                 <input value="4XX" id="cryptogramme-carte-bancaire" disabled/>
-                                
-                                <label class="checkbox-label" for="paiement-3-fois">
-                                <input type="checkbox" name="paiement-3-fois" id="paiement-3-fois" checked disabled>
-                                Paiement en 3 fois
-                                </label>
-                            </div>
-                        </div>
-                        <div class="bulle">
-                            <div class="carte-bancaire">
-                            
-                                <label for="nom-carte-bancaire" style="margin-top:0;">Nom</label>
-                                <input value="Prochaska" id="nom-carte-bancaire" disabled/>
-
-                                <label for="numero-carte-bancaire">Numéro de Carte Bancaire</label>
-                                <input id="numero-carte-bancaire" value="8473 XXXX XXXX XXXX" disabled>
-                            
-                                <label for="cryptogramme-carte-bancaire">Cryptogramme visuel</label>
-                                <input value="2XX" id="cryptogramme-carte-bancaire" disabled/>
-                                
-                                <label class="checkbox-label" for="paiement-3-fois">
-                                <input type="checkbox" name="paiement-3-fois" id="paiement-3-fois" disabled>
-                                Paiement en 3 fois
-                                </label>
-                            </div>
-                        </div>
-                        <div class="bulle">
-                            <div class="carte-bancaire">
-
-                                <label for="nom-carte-bancaire" style="margin-top:0;">Nom</label>
-                                <input value="Xu" id="nom-carte-bancaire" disabled/>
-
-                                <label for="numero-carte-bancaire">Numéro de Carte Bancaire</label>
-                                <input id="numero-carte-bancaire" value="3630 XXXX XXXX XXXX" disabled>
-                            
-                                <label for="cryptogramme-carte-bancaire">Cryptogramme visuel</label>
-                                <input value="6XX" id="cryptogramme-carte-bancaire" disabled/>
                                 
                                 <label class="checkbox-label" for="paiement-3-fois">
                                 <input type="checkbox" name="paiement-3-fois" id="paiement-3-fois" checked disabled>
