3a456017a7b8d6945f7e40cf2927e661ad669618
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-11 10:03:36 +0100

    Debug Recherche.php

--- php/Recherche.php
+++ php/Recherche.php
@@ -42,7 +42,7 @@
             <main>
                 <div class="barre-de-recherche">
                     <div class="barre-de-recheche">
-                        <label for="barre-de-recherche"></label>
+                        <label for="barre-de-recherche">Rechercher</label>
                         <form action="Recherche.php" method="get">
                             <input type="text" name="recherche" placeholder="Nom du produit">
                             <select name="categorieRecherchee">
