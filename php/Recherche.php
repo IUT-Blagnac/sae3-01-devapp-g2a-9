742aed04c1db214de048bb06c8a403e0cd192897
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-01-11 16:49:56 +0100

    Debug recherche.php

--- php/Recherche.php
+++ php/Recherche.php
@@ -162,7 +162,7 @@
                 </div>
                 <?php
                         // permet de desactiver les messages d'erreurs. Enlever des commentaires quand dev fini
-                        // error_reporting(0);
+                        error_reporting(0);
                         if (!is_null($res) or isset($res)) {
                             echo"<div class=\"main-card\">
                             <h2>".$titreRecherche."</h2>
