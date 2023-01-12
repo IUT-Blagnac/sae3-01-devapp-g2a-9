08e1bc02e3c8d8e31fdc8f161c2caafd9f3b78b5
IUT BLAGNAC <thomasdemeyere33@gmail.com>
2023-01-12 12:52:31 +0100

    debug recherceh.php

--- php/Recherche.php
+++ php/Recherche.php
@@ -250,7 +250,7 @@
                                 echo"<script>
                                     document.getElementById(\"cat\").addEventListener(\"change\", function(){
                                         console.log(\"".$res3[0]['cat']."\");
-                                        var nbCategories = ".count($res3)."
+                                        var nbCategories = ".count($res3)."/2;
                                         var catSelected = document.getElementById(\"cat\").value;
                                         document.getElementById(\"sousCat\").options[1].innerHTML = \"".$res3[0]['nom']."\";
                                         console.log(catSelected);
