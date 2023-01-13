aaf8483f64dcaa811af2dfa6431132e8e84fa1e8
IUT BLAGNAC <thomasdemeyere33@gmail.com>
2023-01-13 16:01:26 +0100

    Rien du tout à voir par ici (pas un easter egg)

--- php/Contact.php
+++ php/Contact.php
@@ -14,7 +14,7 @@
                     <p>
                         Adresse : 12 Avenue de la Douze Touldouse</br>
                         Email : <a href="mailto:contact@cdlr.fr" style="text-decoration: underline; color: lightblue;">contact@cdlr.fr</a></br>
-                        Téléphone : <span alt="COPIER" style="color: lightblue; cursor: pointer; text-decoration: underline;" onclick="navigator.clipboard.writeText('+33 4 56 39 82 90');">+33 4 56 39 82 90</span>
+                        <a href="https://www.youtube.com/watch?v=xqnZPHo6qx4">Téléphone :</a><span alt="COPIER" style="color: lightblue; cursor: pointer; text-decoration: underline;" onclick="navigator.clipboard.writeText('+33 4 56 39 82 90');">+33 4 56 39 82 90</span>
                         <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3%20Brasseurs%20Blagnac,%20Rue%20Denis%20Diderot,%2031700%20Blagnac+()&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
                     </p>
                 </div>
