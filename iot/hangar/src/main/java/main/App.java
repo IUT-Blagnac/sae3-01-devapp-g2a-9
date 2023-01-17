a2eb0c118c0d9014429c4dc700ac60aa36eda12f
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-01-17 12:22:26 +0100

    documentation script python faite

--- iot/hangar/src/main/java/main/App.java
+++ iot/hangar/src/main/java/main/App.java
@@ -1,11 +1,12 @@
 package main;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
