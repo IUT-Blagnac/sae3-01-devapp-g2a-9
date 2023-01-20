f9fcf1e79e9a135bdae6913523e993713aa49e29
Anton Xu <anxton4@gmail.com>
2023-01-20 12:05:34 +0100

    delete module info

--- iot/hangar/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires json.simple;
-
-    opens main to javafx.fxml;
-    opens controller to javafx.fxml;
-    exports main;
-}
