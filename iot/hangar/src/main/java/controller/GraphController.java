9d94c82d4a964cdb2948cc2edd307a266f202a3c
Anxton <anxton4@gmail.com>
2023-01-22 20:22:33 +0100

    refactoring code graphcontroller

--- iot/hangar/src/main/java/controller/GraphController.java
+++ iot/hangar/src/main/java/controller/GraphController.java
@@ -39,6 +39,11 @@ public class GraphController implements Initializable {
     @FXML
     private NumberAxis yAxisCO2;
 
+    Integer it;
+    XYChart.Series<String,Integer> tempSeries;
+    XYChart.Series<String,Integer> humSeries;
+    XYChart.Series<String,Integer> co2Series;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Ajout des labels sur l'axe Y des graphs
@@ -47,27 +52,22 @@ public class GraphController implements Initializable {
         yAxisCO2.setLabel("CO2 - ppm");
         
         // Création des séries de données pour les graphes
-        XYChart.Series<String,Integer> tempSeries = new XYChart.Series<String,Integer>();
-        XYChart.Series<String,Integer> humSeries = new XYChart.Series<String,Integer>();
-        XYChart.Series<String,Integer> co2Series = new XYChart.Series<String,Integer>();
+        this.tempSeries = new XYChart.Series<String,Integer>();
+        this.humSeries = new XYChart.Series<String,Integer>();
+        this.co2Series = new XYChart.Series<String,Integer>();
         
         // Ajout des données récupérées depuis le json dans les séries de données
-        Integer iterateur = 1; // Variable permettant de définir la position de la données sur l'axe X des graphes
+        this.it = 1; // Variable permettant de définir la position de la données sur l'axe X des graphes
         JSONObject data = new JSONObject("{\"co2\": [436, false], \"humidity\": [31, true], \"temperature\": [16, false]}"); // La data est convertie d'une string a un objet json
-        addToSeries(data, iterateur, tempSeries, humSeries, co2Series); // ajout des données aux séries de données
-        iterateur += 1;
+        addToSeries(data); // ajout des données aux séries de données
         data = new JSONObject("{\"co2\": [536, false], \"humidity\": [41, true], \"temperature\": [20, false]}");
-        addToSeries(data, iterateur, tempSeries, humSeries, co2Series);
-        iterateur += 1;
+        addToSeries(data);
         data = new JSONObject("{\"co2\": [636, false], \"humidity\": [51, true], \"temperature\": [20, false]}");
-        addToSeries(data, iterateur, tempSeries, humSeries, co2Series);
-        iterateur += 1;
+        addToSeries(data);
         data = new JSONObject("{\"co2\": [536, false], \"humidity\": [61, true], \"temperature\": [22, false]}");
-        addToSeries(data, iterateur, tempSeries, humSeries, co2Series);
-        iterateur += 1;
+        addToSeries(data);
         data = new JSONObject("{\"co2\": [736, false], \"humidity\": [61, true], \"temperature\": [19, false]}");
-        addToSeries(data, iterateur, tempSeries, humSeries, co2Series);
-        iterateur += 1;
+        addToSeries(data);
         
         // Ajout des séries aux graphiques
         chartTemp.getData().add(tempSeries);
@@ -76,15 +76,17 @@ public class GraphController implements Initializable {
     }
     
     // fonction permettant d'ajouter les donées de l'objet json aux séries de données
-    public void addToSeries(JSONObject data, Integer iterateur, XYChart.Series<String,Integer> tempSeries, XYChart.Series<String,Integer> humSeries, XYChart.Series<String,Integer> co2Series){
+    public void addToSeries(JSONObject data){
         int temperature = data.getJSONArray("temperature").getInt(0); // Récupération de la température dans le json
-        tempSeries.getData().add(new XYChart.Data<String,Integer>(iterateur.toString(),temperature)); // Ajout de la température dans la série température
-
+        tempSeries.getData().add(new XYChart.Data<String,Integer>(this.it.toString(),temperature)); // Ajout de la température dans la série température
+        
         int humidite = data.getJSONArray("humidity").getInt(0);
-        humSeries.getData().add(new XYChart.Data<String,Integer>(iterateur.toString(),humidite));
-
+        humSeries.getData().add(new XYChart.Data<String,Integer>(this.it.toString(),humidite));
+        
         int dioxyde = data.getJSONArray("co2").getInt(0);
-        co2Series.getData().add(new XYChart.Data<String,Integer>(iterateur.toString(),dioxyde));
+        co2Series.getData().add(new XYChart.Data<String,Integer>(this.it.toString(),dioxyde));
+        
+        this.it++;
     }
     
 }
